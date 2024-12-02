6185a51dd71e339970044b1a978b9227899f95db
Zeugma324 <noam.pinard.1@ulaval.ca>
2024-12-02 00:37:37 -0500

    adventofcode

---
+++ jour1.py
@@ -0,0 +1,29 @@
+import os
+
+def lire_lignes(fichier):
+    with open(fichier, 'r') as f:
+        lignes = f.readlines()
+    return [ligne.strip() for ligne in lignes]
+
+liste_1 = []
+liste_2 = []
+
+res_1 = 0
+res_2 = 0
+
+for i, ligne in enumerate(lire_lignes("./advent of code 2024/data/data1.txt")):
+    a, b =ligne.split()
+    liste_1.append(int(a))
+    liste_2.append(int(b))
+
+for i in liste_1:
+    res_2 += i * liste_2.count(i)
+
+while liste_1:
+    res_1 += max(min(liste_1),min(liste_2)) - min(min(liste_1),min(liste_2))
+    liste_1.remove(min(liste_1))
+    liste_2.remove(min(liste_2))
+    
+
+print(res_1)
+print(res_2)
---
+++ jour2.py
@@ -0,0 +1,37 @@
+import os
+
+def lire_lignes(fichier):
+    with open(fichier, 'r') as f:
+        lignes = f.readlines()
+    return [ligne.strip() for ligne in lignes]
+
+def bon_niveau(niveau):
+    if niveau == sorted(niveau):
+        if all(1 <= abs(niveau[i] - niveau[i+1]) <= 3 for i in range(len(niveau) - 1)):
+            return True
+    elif niveau == sorted(niveau, reverse=True):
+        if all(1 <= abs(niveau[i] - niveau[i+1]) <= 3 for i in range(len(niveau) - 1)):
+            return True
+    else:
+        return False
+
+niveau = []
+res = 0
+
+for i, ligne in enumerate(lire_lignes("./advent of code 2024/data/data2.txt")):
+    niveau = []
+
+    for d in ligne.split():
+        niveau.append(int(d))
+    if bon_niveau(niveau):
+        res += 1
+    else :
+        for j, niv in enumerate(niveau):
+            temp = niveau[j]
+            niveau.pop(j)
+            if bon_niveau(niveau):
+                res += 1
+                break
+            niveau.insert(j,temp)
+
+print(res)
